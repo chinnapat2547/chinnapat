098ec9aee6cafbc1aadf4c34bfe0f2325f03968a
chinnapat2547 <66010914104@msu.ac.th>
2026-01-27 12:01:04 +0700

    Update connectdb.php

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,8 +1,9 @@
 <?php
-                    $host = "45.144.167.107-732081629";
-					$user = "root";
-					$pws = "chinnapat";
-					$db = "4104db";
-					$conn = mysqli_connect($host, $user, $pws, $db) or die ("Error: " . mysqli_connect_error());
-					mysqli_query($conn, "SET NAMES utf8");
+                    $host = "localhost"; 
+                    $user = "root";
+                    $pws = "chinnapat";
+                    $db = "4104db";
+                    $conn = mysqli_connect($host, $user, $pws, $db) or die ("Error: " . mysqli_connect_error());
+                    
+                    mysqli_query($conn, "SET NAMES utf8");
 ?>
