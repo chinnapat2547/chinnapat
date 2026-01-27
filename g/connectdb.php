a2dfb0936e660c78a19af6948c4e47949ffbabbe
chinnapat2547 <66010914104@msu.ac.th>
2026-01-27 11:47:10 +0700

    Update connectdb.php

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
                     $host = "localhost";
 					$user = "root";
-					$pws = "";
+					$pws = "chinnapat";
 					$db = "4104db";
 					$conn = mysqli_connect($host, $user, $pws, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้"); 
 					mysqli_query($conn, "SET NAMES utf8");
