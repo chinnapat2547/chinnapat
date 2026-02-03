eb58de66152f00126d6fd4842328e503de60e417
Chinnapat <138418837+chinnapat2547@users.noreply.github.com>
2026-02-03 12:03:43 +0700

    Add files via upload

---
+++ h/secure/d.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+
+
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>ชินพัฒน์ ลิ่มดิลกธรรม (คิว)</title>
+</head>
+
+<body>
+<h1>d.php</h1>
+
+<?php
+    unset( $_SESSION['name'] );
+?>
+</body>
+
+</html>
