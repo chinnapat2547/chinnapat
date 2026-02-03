d5ea7fa7b0ff970fda0e9570e5be2db4d8e55e4b
Chinnapat <138418837+chinnapat2547@users.noreply.github.com>
2026-02-03 12:28:38 +0700

    Delete h directory

--- h/secure/connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-                    $host = "localhost";
-					$user = "root";
-					$pws = "";
-					$db = "4104db";
-					$conn = mysqli_connect($host, $user, $pws, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้"); 
-					mysqli_query($conn, "SET NAMES utf8");
-?>
