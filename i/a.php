db43c1415e4e5c3d26318744a167dfbd425b9709
Chinnapat <138418837+chinnapat2547@users.noreply.github.com>
2026-02-10 11:58:59 +0700

    Add files via upload

--- i/a.php
+++ i/a.php
@@ -5,6 +5,47 @@
 <title>ชินพัฒน์ ลิ่มดิลกธรรม(คิว)</title>
 </head>
 
-<body><h1>งานI </h1> ชินพัฒน์ ลิ่มดิลกธรรม(คิว)
+<body><h1> ข้อมูลภาค ชินพัฒน์ ลิ่มดิลกธรรม(คิว) </h1> 
+
+<form method="post" action="">
+	ชื่อภาค <input type="text" name="rname" autofocus required>
+	<button type="submit" name="Submit">บันทึก</button>
+</form>
+<br>
+<br>
+
+<?php 
+if(isset($_POST['Submit'])){
+	include_once("connectdb.php");
+	$rname = $_POST['rname'];
+	$sql2 = "INSERT INTO `regions` VALUES (NULL, '$rname')";
+	mysqli_query($conn, $sql2) or die ("insert ไม่ได้");
+}
+?>
+
+
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+    </tr>
+
+<?php
+    include_once("connectdb.php");
+    $sql = "SELECT * FROM `regions` ORDER BY `regions`.`r_id` ASC";
+    $rs = mysqli_query($conn, $sql);
+    
+    while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+    </tr>
+<?php
+    } 
+?>
+
+</table>
+
 </body>
 </html>
